5d3c8d6bb3517a5ead5870b7461a2a9983a6f688
Zulfikharali Muzakir <zulfikar26error@gmail.com>
2026-02-15 22:49:27 +0700

    feat: initial project setup for oxydoc CLI

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use anyhow::Ok;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(name = "oxydoc")]
+#[command(version = "0.1.0")]
+#[command(about = "Convert OpenAPI Specification to PDF", long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    input: PathBuf,
+
+    #[arg(short, long, value_name = "FILE")]
+    output: PathBuf,
+
+    #[arg(short, long, default_value = "a4")]
+    page_size: String,
+
+    #[arg(long)]
+    include_schemas: bool,
+
+    #[arg(long)]
+    include_examples: bool,
+
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+
+    println!("Input : {:?}", args.input);
+    println!("Output : {:?}", args.output);
+    println!("Page Size : {}", args.page_size);
+    println!("Include Schemas : {}", args.include_schemas);
+    println!("Include Examples : {}", args.include_examples);
+    println!("Verbose : {}", args.verbose);
+
+    Ok(())
+}
